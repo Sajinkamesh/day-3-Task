7e3f853e80a4b8282214173d60d0bc534e760e8e
Sajin Kumar D <110959439+Sajinkamesh@users.noreply.github.com>
2022-10-20 07:02:33 -0700

    Add files via upload
    This is day 3 Task : B41WD Tamil

---
+++ day3.js
@@ -0,0 +1,29 @@
+
+var Resume = [{"Objective":"A creative support professional with a record of increase responsibility. Proficient in prioritizing and completing tasks in a timely manner, yet flexible to multitasking when necessary. Customer focused with diverse industry experience. Enjoys learning new programs and processes. A team player who is attentive to detail and able to wok in a fast pace environment. Also excellent oral and written communication skills."
+
+},
+    
+ Profile =  {"Name": "sajin Kumar" , 
+                "DOB": "18/05/1991",
+                "Father name": "Devasahayam",
+                
+                "Sex": "Male",
+                "Nationality": "Indian",
+                "Language": "Tamil,English, Malayalam"},
+
+Address = {"Place":"Azhagiamandapam",
+            "Post": "Mulagumoodu",
+            "Taluk": "Kalkulam",
+            "Dist": "Kanyakumari" },
+ Education = {
+             "Qualification": "B.E.Mechanical Engg",
+             "Year of passing": "2019",
+             "Percentage": "68",
+}
+
+]
+
+console.log(Resume[0]);
+console.log(Profile);
+console.log(Address);
+console.log(Education);
---
+++ forloop.js
@@ -0,0 +1,25 @@
+//for loop
+let Names =["sajin","shaji","ajin","anto","sujin"]
+
+for(let i = 0; i<Names.length; i++){
+    console.log(Names[i]);
+}
+//for in loop
+let age =["5","10","20","28","31"]
+
+for(let key in age){
+    console.log(key,age[key])
+}
+//for of loop
+let sex =["Male","Female","Trans","yyy","zzz"]
+
+    for(let obj of sex){
+        console.log(obj)
+    }
+//for Each loop
+let fruites =["Apple","Orenge","Grape","Banana"]
+
+fruites.forEach(Frname => {
+    console.log(fruites)
+}
+)
